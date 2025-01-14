a7b98b94caf0f598f213718bab0c64336db2bc4a
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-01-14 14:19:06 +0100

    Invalid code showing the lifetime of two variables

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,31 +1,14 @@
-// Use the & to indicate a borrow that can be mutated... 
-fn display(s: &mut String) {
-    println!("{}", s);
-}
-
-// The rule of the borrowing... validated at compile time by borrow checker
-// - one mutable borrow OR any number of immutable borrow. 
-// - the borrow checker insure that the borrowed reference does not survive to its borrower
-
+// Show invalid code because of wrong lifetime of variable pointed by 
+// borrowers 
 fn main()
 {
-    // felix has the ownership of the string, flaggued as mutable... 
-    let mut s = String::from("A cat");
+    let r;
 
-    // create a local context
     {
-        // two immutable borrow, valid
-        let borrow = &s;
-        let borrow2 = &s;
-        println!("{},{}", borrow, borrow2);
-        
-        // not possible to do:
-        // let mborrow = &mut s; 
+        let x = 5;
+        r = &x; 
     }
 
-
-    display(&mut s);
-
     // The following does not work because 
-    println!("Tom {}!\n", s);
+    println!("Tom {} {}!\n", r, x);
 }
