5779015660433f74b2db48c3b7379ea7842ff470
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-01-14 14:24:56 +0100

    Lifetime annotation.

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,8 +1,10 @@
-fn longest(x: &str, y: &str) -> &str{
+// Life time annotation, 'a ... then x and y have the smallest lifetime 
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
     if x.len() > y.len(){
         x
-    }  
-    y
+    }else{  
+        y
+    }
 }
 
 // Show invalid code because of wrong lifetime of variable pointed by 
@@ -12,7 +14,7 @@ fn main()
     let s1 = String::from("abcd");
     let s2 = "xyz";
 
-    let result = longest(strng1.as_str(), string2);
+    let result = longest(s1.as_str(), s2);
 
     // The following does not work because 
     println!("Tom {} !\n", result);
