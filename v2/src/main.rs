4e52a9d54c75238bcaf2f85a23d83f20abcb55d4
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-01-14 14:13:59 +0100

    Add comment to borrowing.

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,15 +1,19 @@
-// Use the & to indicate a borrow... 
-fn display(s: &String) {
+// Use the & to indicate a borrow that can be mutated... 
+fn display(s: &mut String) {
     println!("{}", s);
 }
 
+// The rule of the borrowing... validated at compile time by borrow checker
+// - one mutable borrow OR any number of immutable borrow. 
+// - the borrow checker insure that the borrowed reference does not survive to its borrower
+
 fn main()
 {
-    // felix has the ownership of the string
-    let felix = String::from("A cat");
+    // felix has the ownership of the string, flaggued as mutable... 
+    let mut felix = String::from("A cat");
 
-    display(&felix);
+    display(&mut felix);
 
     // The following does not work because 
-    //println!("Tom {}!\n", felix);
+    println!("Tom {}!\n", felix);
 }
