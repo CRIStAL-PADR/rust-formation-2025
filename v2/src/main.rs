bed95c6ecaee645af528a196f395b65901a5d5c6
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-01-14 14:08:47 +0100

    Move to cargo (needed to get intellisense)

---
+++ v2/src/main.rs
@@ -0,0 +1,14 @@
+fn display(s: String) {
+    println!("{}", s);
+}
+
+fn main()
+{
+    // felix has the ownership of the string
+    let felix = String::from("A cat");
+
+    display(felix);
+
+    // The following does not work because 
+    //println!("Tom {}!\n", felix);
+}
