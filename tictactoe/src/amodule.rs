42cb3daa1889865d8b7129d1ba771ba367604450
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-01-14 15:12:55 +0100

    Add a module...

---
+++ tictactoe/src/amodule.rs
@@ -0,0 +1,13 @@
+// A module is:
+//    a mod mod_name {} scope in a normal rust file
+//    a independ file (.rs)
+//    a folder that contains a mod.rs file. 
+
+
+// Ceci est un module 
+mod a_module 
+{
+
+
+}
+
